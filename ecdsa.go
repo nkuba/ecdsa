6df6021dffacff204f4423a23a3b9dbcdc630c63
Jakub Nowakowski <jakub.nowakowski@akena.engineering>
2018-10-15 18:07:13 +0200

    Signature generation

--- ecdsa.go
+++ ecdsa.go
@@ -3,6 +3,9 @@ package tecdsa
 import (
 	"crypto/sha256"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+	"github.com/golang/go/src/crypto/rand"
 )
 
 type Signature struct {
@@ -11,6 +14,48 @@ type Signature struct {
 
 type Point struct {
 	X, Y *big.Int
+}
+
+func Sign(curve *secp256k1.BitCurve, privateKey *big.Int, message []byte) (*Signature, error) {
+	z := hashAndTrim(message, curve.BitSize)
+
+	var k, r, s *big.Int
+	for {
+		for {
+			for {
+				var err error
+				k, err = rand.Int(rand.Reader, curve.N)
+				if err != nil {
+					return nil, err
+				}
+				if k.Sign() > 0 {
+					break
+				}
+			}
+
+			Xp, _ := curve.ScalarBaseMult(k.Bytes())
+
+			r = new(big.Int).Mod(Xp, curve.N)
+			if r.Sign() != 0 {
+				break
+			}
+		}
+		s = new(big.Int).Mod(
+			new(big.Int).Mul(
+				new(big.Int).ModInverse(k, curve.N),
+				new(big.Int).Add(
+					z,
+					new(big.Int).Mul(r, privateKey),
+				),
+			),
+			curve.N,
+		)
+		if s.Sign() != 0 {
+			break
+		}
+	}
+
+	return &Signature{R: r, S: s}, nil
 }
 
 func hashAndTrim(message []byte, bitLen int) *big.Int {
